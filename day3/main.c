e574cf52dcb8f8ab269126efdeaac7f2d0aeb9ec
OtakuGod0 <130218032+OtakuGod0@users.noreply.github.com>
2023-07-11 00:40:12 +0000

    day 3

--- day3/main.c
+++ day3/main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-int true(){
-    return 1;
-}
-
 int main(){
-    if(true()){
-        printf("Heloooo world!!!\n");
+    float a = 0.1 , b = 0.3;
+    float x = a+b;
+    printf("%.30f\n", x);
 
-    }
-    char x = "0";
-    
     return 0;
 }
--- day3/taking_no_argument_returns.c
+++ day3/taking_no_argument_returns.c
@@ -9,8 +9,5 @@ int main(){
         printf("Heloooo world!!!\n");
         
     }
-    float a = 0.1 , b = 0.3;
-    float x = a+b;
-    printf("%.30f\n", x);
     return 0;
 }
