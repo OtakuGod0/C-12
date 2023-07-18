81a144d099f6d5c256f02d7741a237b2f54dfc9e
OtakuGod0 <130218032+OtakuGod0@users.noreply.github.com>
2023-07-18 01:56:18 +0000

    day5

--- day4/main.c
+++ day4/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int * ret(){
+int *arr(){
     static int x[10] = {1, 3, 1};
     return x;
 }
 int main(){
-    int *n = ret(), i;
+    int *n = arr(), i;
     for(i=0;i<3;i++) printf("%d\n",  n[i]);
     return 0;
 }
