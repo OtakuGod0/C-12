fecfc3a448b2214d0c1c5ca68f2fe0cafaefb040
OtakuGod0 <130218032+OtakuGod0@users.noreply.github.com>
2023-07-18 01:21:07 +0000

    day5

---
+++ day5/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//function declaration
+int pcheck(int);
+int main()
+{
+    //vairable declaration
+    int n, i;
+
+    //taking input
+    printf("Enter one number: ");
+    scanf("%d", &n);
+
+    //looping through n to 1 
+    for(i=n;i>=1;i--){
+        //checking if i is prime and if true printing i
+        if(pcheck(i))
+            printf("%d\n", i);
+    }
+
+    //termination
+    return 0;
+}
+
+//function defination
+//function that returns 1 if prime 0 if composite
+int pcheck(int x){
+    //local vairable declaration
+    int n, d=0;
+
+    //looping through n to 1 and if any deviser counter d is incremented
+    for(n=x;n>=1;n--){
+        if(x%n==0){
+            d++;
+        }
+    }
+
+    //checking if number of deviser from n to 1 is greater then 2 if yes returns 0 if not returns 1
+    if(d>2) return 0;
+    else return 1;
+
+}
